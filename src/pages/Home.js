a86df625f4dff36a43e7f5da13734e534ce08a19
kitgragasin <kitgragasin@gmail.com>
2023-03-02 00:57:18 +0800

    Fixed Table Headers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { useNavigate, Link} from "react-router-dom";
+import {  Link} from "react-router-dom";
 
 export default function Home() {
   const [city, setCity] = useState('');
@@ -7,32 +7,15 @@ export default function Home() {
     setCity(event.target.value);
     console.log("city is ",city)
   };
-  let navigate = useNavigate;
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("e",e);
-    console.log("submit city ",city);
-    navigate(
-      '/weather',
-      {state:{
-        city:city
-      }}
-    );
-  };
-
 
   return (
     <div>
       <p>Home</p>
-    {//  <form onSubmit={handleSubmit}>
-    }
         <label>
           Enter a city name:
           <input type="text" value={city} onChange={handleCityChange} />
         </label>
         <button type="submit"><Link to="/weather" state= {{city:city}}>Get Weather</Link></button>
-    {// </form>
-      }
     </div>
   )
 }
