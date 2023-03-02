f57a6d915b082604b488c3b88b247ae91acd9fb6
kitgragasin <kitgragasin@gmail.com>
2023-03-02 09:42:50 +0800

    Added Styling to Weather App

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,37 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import {Link } from "react-router-dom";
 import LogoutButton from "./components/LogoutButton";
+import { Menu } from '@mui/icons-material';
+
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Landing</Link>
-        </li>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/weather">Weather</Link>
-        </li>
-        <li>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <Menu/>
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link style={{ color: '#FFF' }} to="/home">Home</Link>
+            </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link style={{ color: '#FFF' }} to="/weather">Weather</Link>
+            </Typography>
             <LogoutButton/>
-        </li>
-      </ul>
-    </nav>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,31 @@
 import React, {useState} from 'react'
-import {  Link, redirect} from "react-router-dom";
+import {  Link} from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Container, TextField } from '@mui/material';
 
 export default function Home() {
   const [city, setCity] = useState('');
   const { user, isAuthenticated } = useAuth0();
   const handleCityChange = (event) => {
     setCity(event.target.value);
-    console.log("city is ",city)
   };
 
-  console.log(isAuthenticated)
-  // if(!isAuthenticated){
-  //   console.log('is not authenticated')
-  //   return redirect('/');
-  // }
   return (
     <div>
-      <p>Home</p>
+      <Container maxWidth="sm">
+      <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <p>
         <label>
-          Enter a city name:
-          <input type="text" value={city} onChange={handleCityChange} />
+          <TextField id="outlined-basic" label="City" variant="outlined" value={city} onChange={handleCityChange}>
+          </TextField>
+          
         </label>
-        <button type="submit"><Link to="/weather" state= {{city:city}}>Get Weather</Link></button>
+        </p>
+        <Button variant="contained" color="inherit" type="submit"><Link to="/weather" state= {{city:city}}>Get Weather</Link></Button>
+      </Container>
+
     </div>
   )
 }
